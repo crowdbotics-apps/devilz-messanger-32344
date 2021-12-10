3d42c90f5ab081a6bf792184d06eeaa258faf405
Crowdbotics <team@crowdbotics.com>
2021-12-10 18:16:18 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/devilz_messanger_32344/MainActivity.java
@@ -0,0 +1,15 @@
+package com.devilz_messanger_32344;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "devilz_messanger_32344";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "devilz_messanger_32344",
+  url: "https://devilz_messanger_32344.botics.co",
+  api: "https://devilz_messanger_32344.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
